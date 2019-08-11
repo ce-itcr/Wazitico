a0cbb965b20aa0a20d5dce8aa631f00e43e00c10
Jonathan Esquivel <jesquivelsa@gmail.com>
2019-08-10 18:36:36 -0600

    [UPDATE] Node Amounts

--- Wazitico.rkt
+++ Wazitico.rkt
@@ -1,4 +1,4 @@
 #lang racket/gui
 (require racket/gui/base)
 
-(define graph '((a ()) (b ()) (c ()) (d ()) (e ()) (f ()) (g ()) (h ()) (i ()) (j ()) (k ()) (l ()) (m ()) (n ()) (o ()) (p ()) (q ()) (r ()) (s ()) (t ())))
+(define graph '((a ()) (b ()) (c ()) (d ()) (e ()) (f ()) (g ()) (h ()) (i ()) (j ()) (k ()) (l ()) (m ()) (n ()) (o ()) (p ()) (q ()) (r ()) (s ()) (t ()) (u ()) (v ()) (w ()) (x ()) (y ())))
