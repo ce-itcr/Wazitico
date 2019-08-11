4b80b1f7ba413160219057d4c6f0465e986633c1
Jonathan Esquivel <jesquivelsa@gmail.com>
2019-08-10 19:07:22 -0600

    [UPDATE] Graph Even

--- Wazitico.rkt
+++ Wazitico.rkt
@@ -1,4 +1,4 @@
 #lang racket/gui
 (require racket/gui/base)
 
-(define graph '((a ()) (b ()) (c ()) (d ()) (e ()) (f ()) (g ()) (h ()) (i ()) (j ()) (k ()) (l ()) (m ()) (n ()) (o ()) (p ()) (q ()) (r ()) (s ()) (t ()) (u ()) (v ()) (w ()) (x ()) (y ())))
+(define graph '((a (b f)) (b ()) (c (b d h)) (d ()) (e (d j)) (f ()) (g (b h)) (h ()) (i (d j)) (j ()) (k (f p)) (l ()) (m (h l r)) (n ()) (o (j n t)) (p ()) (q (l r)) (r ()) (s (n t)) (t ()) (u (p v)) (v ()) (w (v x)) (x ()) (y (t x))))
