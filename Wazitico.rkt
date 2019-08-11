6b8ae47a7747822779ad39babee364926c531150
Iván <isolis@solavcr.com>
2019-08-10 18:19:48 -0600

    [UPDATE] Imports

--- Wazitico.rkt
+++ Wazitico.rkt
@@ -1,2 +1,4 @@
-#lang racket
+#lang racket/gui
+(require racket/gui/base)
+
 (define graph '((a ()) (b ()) (c ()) (d ()) (e ()) (f ()) (g ()) (h ()) (i ()) (j ()) (k ()) (l ()) (m ()) (n ()) (o ()) (p ()) (q ()) (r ()) (s ()) (t ())))
