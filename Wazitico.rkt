d039f65d81643e5e8887979208b397494ac19928
Iván <isolis@solavcr.com>
2019-08-10 19:05:50 -0600

    [UPDATE] Graph odds

--- Wazitico.rkt
+++ Wazitico.rkt
@@ -1,4 +1,4 @@
 #lang racket/gui
 (require racket/gui/base)
 
-(define graph '((a ()) (b ()) (c ()) (d ()) (e ()) (f ()) (g ()) (h ()) (i ()) (j ()) (k ()) (l ()) (m ()) (n ()) (o ()) (p ()) (q ()) (r ()) (s ()) (t ()) (u ()) (v ()) (w ()) (x ()) (y ())))
+(define graph '((a ()) (b (a c)) (c ()) (d (c e)) (e ()) (f (a g k)) (g ()) (h (i m)) (i ()) (j (e o)) (k ()) (l (g k)) (m ()) (n (m i)) (o ()) (p (q u)) (q ()) (r (s w)) (s ()) (t (o y)) (u ()) (v (u q w)) (w ()) (x (w s y)) (y ())))
