86fb932881f8faeb92585b264af110c36d7e18ff
Angelo Ortiz Vega <angelo2898@gmail.com>
2019-08-15 20:12:10 -0600

    [UPDATE] Find all paths using width first

--- src/pathfind/finder.rkt
+++ src/pathfind/finder.rkt
@@ -1,5 +1,29 @@
 #lang racket
 
+(require "../util/basic_operations.rkt")
+(provide find-paths)
 
 
+;;Find all routes between src and dest in a graph
+;;Function uses width first
+(define (find-paths src dest graph)
+  (cond ((equal? src dest)
+         (list src))
+        (else
+         (find-paths-aux (list (list src)) dest graph '()))))
+
+(define (find-paths-aux paths end graph result)
+  (cond ((null? paths)
+         (reverse-sublists result))
+        ((equal? end (car paths))
+         (find-paths-aux (cdr paths)
+                         end
+                         graph
+                         (cons (car paths) result)))
+        (else
+         (find-paths-aux (append (extend (car paths) graph)
+                                 (cdr paths))
+                         end
+                         graph
+                         result))))
 
